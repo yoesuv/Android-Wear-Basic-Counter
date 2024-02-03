9d9e638d770f217ca0d9ad6d841c76cf0beeef3c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-02-03 10:43:25 +0700

    :art: ui mobile apps

--- app/src/main/java/com/yoesuv/basiccounter/MainActivity.kt
+++ app/src/main/java/com/yoesuv/basiccounter/MainActivity.kt
@@ -10,13 +10,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,15 +47,23 @@ class MainActivity : ComponentActivity() {
                             fontWeight = FontWeight.Medium,
                             fontSize = 24.sp
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(16.dp))
                         Text(text = "0", fontWeight = FontWeight.Bold, fontSize = 120.sp)
                         Row {
                             Button(onClick = { /*TODO*/ }) {
-
+                                Icon(
+                                    imageVector = Icons.Default.Remove,
+                                    contentDescription = null,
+                                    tint = Color.White
+                                )
                             }
                             Spacer(modifier = Modifier.width(8.dp))
                             Button(onClick = { /*TODO*/ }) {
-
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = Color.White
+                                )
                             }
                         }
                     }
