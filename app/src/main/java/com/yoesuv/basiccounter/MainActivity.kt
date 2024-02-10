b0cf5b0076c62dd4d384f5e1f52246fd2af7eb55
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-02-10 11:06:23 +0700

    :sparkles: on data changed listener

--- app/src/main/java/com/yoesuv/basiccounter/MainActivity.kt
+++ app/src/main/java/com/yoesuv/basiccounter/MainActivity.kt
@@ -37,6 +37,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel.init(this)
+
         setContent {
             BasicCounterTheme {
                 Surface(
--- app/src/main/java/com/yoesuv/basiccounter/MainViewModel.kt
+++ app/src/main/java/com/yoesuv/basiccounter/MainViewModel.kt
@@ -1,11 +1,24 @@
 package com.yoesuv.basiccounter
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.wearable.DataClient
+import com.google.android.gms.wearable.DataEventBuffer
+import com.google.android.gms.wearable.Wearable
+import com.yoesuv.basiccounter.source.Constants
+import java.nio.charset.StandardCharsets
 
-class MainViewModel: ViewModel() {
+class MainViewModel: ViewModel(), DataClient.OnDataChangedListener {
 
+    private lateinit var dataClient: DataClient
     var counter: MutableLiveData<Int> = MutableLiveData(0)
+
+    fun init(context: Context) {
+        dataClient = Wearable.getDataClient(context)
+        dataClient.addListener(this)
+    }
 
     fun add() {
         val current = counter.value ?: 0
@@ -15,6 +28,19 @@ class MainViewModel: ViewModel() {
     fun subtract() {
         val current = counter.value ?: 0
         counter.postValue(current - 1)
+    }
+
+    override fun onDataChanged(dataEvents: DataEventBuffer) {
+        dataEvents.forEach { dataEvent ->
+            if (dataEvent.dataItem.data != null) {
+                val uri = dataEvent.dataItem.uri
+                if (uri.path == Constants.COUNT_PATH) {
+                    val data = dataEvent.dataItem.data!!
+                    val strCount = String(data, StandardCharsets.UTF_8)
+                    Log.d(Constants.TAG_DEBUG, "MainViewModel # onDataChanged: $strCount")
+                }
+            }
+        }
     }
 
 }
