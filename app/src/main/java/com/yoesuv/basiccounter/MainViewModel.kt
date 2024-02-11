38bd3c02309f7d0f93af2519f3bbe7e6b24938c2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-02-11 20:57:23 +0700

    :sparkles: sync data

--- app/src/main/java/com/yoesuv/basiccounter/MainViewModel.kt
+++ app/src/main/java/com/yoesuv/basiccounter/MainViewModel.kt
@@ -6,11 +6,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEventBuffer
+import com.google.android.gms.wearable.PutDataRequest
 import com.google.android.gms.wearable.Wearable
 import com.yoesuv.basiccounter.source.Constants
+import com.yoesuv.basiccounter.source.Constants.COUNT_PATH
+import com.yoesuv.basiccounter.source.Constants.TAG_DEBUG
+import com.yoesuv.basiccounter.source.Constants.TAG_ERROR
 import java.nio.charset.StandardCharsets
 
-class MainViewModel: ViewModel(), DataClient.OnDataChangedListener {
+class MainViewModel : ViewModel(), DataClient.OnDataChangedListener {
 
     private lateinit var dataClient: DataClient
     var counter: MutableLiveData<Int> = MutableLiveData(0)
@@ -22,19 +26,32 @@ class MainViewModel: ViewModel(), DataClient.OnDataChangedListener {
 
     fun add() {
         val current = counter.value ?: 0
-        counter.postValue(current + 1)
+        counter.value = current + 1
+        send()
     }
 
     fun subtract() {
         val current = counter.value ?: 0
-        counter.postValue(current - 1)
+        counter.value = current - 1
+        send()
+    }
+
+    private fun send() {
+        val counter = this.counter.value ?: 0
+        val request = PutDataRequest.create(Constants.COUNT_PATH)
+        request.setData("$counter".toByteArray())
+        dataClient.putDataItem(request.setUrgent()).addOnSuccessListener {
+            Log.d(TAG_DEBUG, "MainViewModel # SUCCESS SEND DATA ${it.data}")
+        }.addOnFailureListener {
+            Log.e(TAG_ERROR, "MainViewModel # ERROR $it")
+        }
     }
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
         dataEvents.forEach { dataEvent ->
             if (dataEvent.dataItem.data != null) {
                 val uri = dataEvent.dataItem.uri
-                if (uri.path == Constants.COUNT_PATH) {
+                if (uri.path == COUNT_PATH) {
                     val data = dataEvent.dataItem.data!!
                     val strCount = String(data, StandardCharsets.UTF_8)
                     val intCount = strCount.toInt()
--- basic-counter-wear/src/main/java/com/yoesuv/basiccounter/wear/presentation/MainWearViewModel.kt
+++ basic-counter-wear/src/main/java/com/yoesuv/basiccounter/wear/presentation/MainWearViewModel.kt
@@ -5,19 +5,21 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.wearable.DataClient
+import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.PutDataRequest
 import com.google.android.gms.wearable.Wearable
 import com.yoesuv.basiccounter.source.Constants.COUNT_PATH
 import com.yoesuv.basiccounter.source.Constants.TAG_DEBUG
 import com.yoesuv.basiccounter.source.Constants.TAG_ERROR
 
-class MainWearViewModel: ViewModel() {
+class MainWearViewModel : ViewModel(), DataClient.OnDataChangedListener {
 
     private lateinit var dataClient: DataClient
     var counter: MutableLiveData<Int> = MutableLiveData(0)
 
     fun init(context: Context) {
         dataClient = Wearable.getDataClient(context)
+        dataClient.addListener(this)
     }
 
     fun add() {
@@ -32,14 +34,35 @@ class MainWearViewModel: ViewModel() {
         send()
     }
 
-    private fun send () {
+    private fun send() {
         val counter = this.counter.value ?: 0
         val request = PutDataRequest.create(COUNT_PATH)
         request.setData("$counter".toByteArray())
         dataClient.putDataItem(request.setUrgent()).addOnSuccessListener {
-            Log.d(TAG_DEBUG,"MainWearViewModel # SUCCESS SEND DATA ${it.data}")
+            Log.d(TAG_DEBUG, "MainWearViewModel # SUCCESS SEND DATA ${it.data}")
         }.addOnFailureListener {
             Log.e(TAG_ERROR, "MainWearViewModel # ERROR $it")
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dataClient.removeListener(this)
+    }
+
+    override fun onDataChanged(dataEvents: DataEventBuffer) {
+        Log.d(TAG_DEBUG, "MainWearViewModel # onDataChanged: $dataEvents")
+        dataEvents.forEach { dataEvent ->
+            if (dataEvent.dataItem.data != null) {
+                val uri = dataEvent.dataItem.uri
+                if (uri.path == COUNT_PATH) {
+                    val data = dataEvent.dataItem.data!!
+                    val strCount = String(data, Charsets.UTF_8)
+                    val intCount = strCount.toInt()
+                    Log.d(TAG_DEBUG, "MainWearViewModel # onDataChanged: $strCount")
+                    counter.postValue(intCount)
+                }
+            }
         }
     }
 
